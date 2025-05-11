9bd914895a8d56a1551e5eb1e442a2317e7cdb0d
Ron Almog <ron8132@gmail.com>
2025-05-10 21:49:07 -0400

    upgrade to 3.37

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,20 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default withPayload(nextConfig, { devBundleServerPackages: false })
