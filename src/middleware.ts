3d9e19447b858d7ade2e1e86f2aab04c15f22895
Ron Almog <ron8132@gmail.com>
2025-05-12 10:55:31 -0400

    remove api from middleware

---
+++ src/middleware.ts
@@ -0,0 +1,34 @@
+import { NextResponse } from 'next/server'
+import { NextRequest } from 'next/server'
+
+export async function middleware(request: NextRequest) {
+  // check for cookie
+  const payloadToken = request.cookies.get('payload-token')
+  if (!payloadToken) {
+    return NextResponse.redirect(new URL('/sign-in', request.url))
+  }
+
+  // TODO: a cookie is not enough. you should check for real user.
+  // prevent / if needed
+  //   if (request.nextUrl.pathname === '/') {
+  //     return NextResponse.redirect(new URL('/home', request.url))
+  //   }
+
+  // check things based on cookie, like admin/user
+  // ...
+
+  // let payload handle /api routes
+  if (request.nextUrl.pathname.startsWith('/api')) {
+    return NextResponse.next()
+  }
+
+  // let server components handle token verification
+  return NextResponse.next()
+}
+
+// this code dictates which routes the middleware will run on
+// aka, protected routes. for logged in users only.
+// removing '/api/:path*' as it is blocking the admin user from login
+export const config = {
+  matcher: ['/dashboard', '/todos/:path*'],
+}
